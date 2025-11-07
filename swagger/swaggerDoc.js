358ea5a6341fa57695a67a6bc70e722974af8844
madiha <madihaelyazal29@gmail.com>
2025-11-07 17:48:02 +0200

    Update swaggerDoc.js

--- swagger/swaggerDoc.js
+++ swagger/swaggerDoc.js
@@ -1,19 +1,23 @@
-const swaggerJDoc=require('swagger-jsdoc');
-const swaggerUI=require('swagger-ui-express');
-const options={
-    definitions:{
-        openapi:'3.0.0',
-        info:{
-            title:'Metro Mate Api Documentation',
-            version:'1.0.0',
-            description:'Api document for metro project'
-        }
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+const path = require("path");
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Metro Mate API Documentation",
+      version: "1.0.0",
+      description: "API document for metro project",
     },
-    apis:[__dirname+"/userDoc.js",__dirname+"/../routes/*.js"],
+  },
+  apis: [
+    path.join(__dirname, "userDoc.js"),
+    path.join(__dirname, "../routes/*.js"),
+  ],
 };
-const swaggerSpec=swaggerJDoc(options);
-function swaggerDoc(app){
-    app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerSpec));
-     console.log(" Swagger Docs available at ");
+const swaggerSpec = swaggerJSDoc(options);
+function swaggerDoc(app) {
+  app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+  console.log("Swagger Docs available at /api-docs");
 }
-module.exports=swaggerDoc;
+module.exports = swaggerDoc;
