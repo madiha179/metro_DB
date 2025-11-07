bd133139241d8ae0f8abfcdb850ab2b38517e2f5
madiha <madihaelyazal29@gmail.com>
2025-11-07 17:23:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 const express=require('express');
+const swaggerDocs=require('./swagger/swaggerDoc.js')
+const userRouter=require('./routes/userRoute')
 dotenv.config({path:'config.env'});
 const app=express();
 app.use(express.json());
-const port=process.env.PORT;
+const port = process.env.PORT|| 3000;
 const DB=process.env.DATABASE;
-mongoose.connect(DB)
+mongoose.connect(DB,{
+   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
+})
 .then(() => console.log("✅ DB connection successful"))
   .catch(err => console.error("❌ DB connection error:", err));
+  swaggerDocs(app);
+  app.use('/api/v1/users',userRouter);
 app.listen(port,()=>{
     console.log("working on 3000")
 });
