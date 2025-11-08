83f04e6aad5548e1f46f329c0c58db80f43a919c
madiha <madihaelyazal29@gmail.com>
2025-11-09 00:33:48 +0200

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,68 +1,57 @@
 const AppError = require('./../utils/appError');
-
-// user entered wrong data type
-const handleCastErrorDB = err => {
-  const message = `Invalid ${err.path}: ${err.value}`;
-  return new AppError(message, 400);
-};
-
-// user entered duplicate unique field like enter an exist email
-const handleDuplicateFieldsDB = err => {
-  const field = Object.keys(err.keyValue)[0];
-  const value = err.keyValue[field];
-  const message = `Duplicate field value: '${value}'. Please use another ${field}!`;
-  return new AppError(message, 400);
-};
-
-// schema validation failed => make sure all user inputs match conditions in DB
-const handleValidationErrorDB = err => {
-  const errors = Object.values(err.errors).map(el => el.message);
-  const message = `Invalid input data. ${errors.join('. ')}`;
-  return new AppError(message, 400);
-};
-
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack
-  });
-};
-
-const sendErrorProd = (err, res) => {
-  // Operational, trusted error: send message to client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-  } else {
-    // Programming or other unknown error: don't leak error details
-    console.error('ERROR', err);
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong!'
-    });
-  }
-};
-
-// global error handler
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
-    let error = { ...err }; // Spread operator
-    error.message = err.message;
-    // MongoDB CastError (invalid ID)
-    if (err.name === 'CastError') error = handleCastErrorDB(err);
-    // MongoDB Duplicate key error
-    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
-    // MongoDB Validation error
-    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+  console.log('Error Details:', {
+    name: err.name,
+    code: err.code,
+    message: err.message,
+    keyValue: err.keyValue
+  });
 
-    sendErrorProd(error, res);
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+  } else {
+    if (err.code === 11000) {
+      const field = Object.keys(err.keyValue)[0];
+      const value = err.keyValue[field];
+      return res.status(400).json({
+        status: 'fail',
+        message: `Duplicate field value: '${value}'. Please use another ${field}!`
+      });
+    }
+
+    if (err.name === 'CastError') {
+      return res.status(400).json({
+        status: 'fail',
+        message: `Invalid ${err.path}: ${err.value}`
+      });
+    }
+
+    if (err.name === 'ValidationError') {
+      const errors = Object.values(err.errors).map(el => el.message);
+      const message = `Invalid input data. ${errors.join('. ')}`;
+      return res.status(400).json({
+        status: 'fail',
+        message: message
+      });
+    }
+    if (err.isOperational) {
+      res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+      });
+    } else {
+      console.error('ERROR', err);
+      res.status(500).json({
+        status: 'error',
+        message: 'Something went wrong!'
+      });
+    }
   }
 };
