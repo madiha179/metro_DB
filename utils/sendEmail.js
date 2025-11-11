bc9aecbd0aa5661841a5f4126d9f11067b42f0e9
madiha <madihaelyazal29@gmail.com>
2025-11-11 21:41:54 +0200

    try solve

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -11,40 +11,38 @@ module.exports = class Email {
     this.from = process.env.EMAIL_FROM;
   }
 
-  newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      // Production: SendGrid Web API
-      sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-      return sgMail;
-    }
-
-    // Development: Mailtrap or local SMTP
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
-  }
-
   async send(template, subject) {
     let html = fs.readFileSync(`${__dirname}/../views/emails/${template}.html`, 'utf-8');
     html = html.replace('{{firstName}}', this.firstName).replace('{{url}}', this.url);
 
     const mailOptions = {
-      from: this.from,
       to: this.to,
+      from: this.from,
       subject,
       html,
       text: htmlToText.convert(html)
     };
 
-    if (process.env.NODE_ENV === 'production') {
-      await this.newTransport().send(mailOptions);
-    } else {
-      await this.newTransport().sendMail(mailOptions);
+    try {
+      if (process.env.NODE_ENV === 'production') {
+        // SendGrid Web API
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        await sgMail.send(mailOptions);
+      } else {
+        // Development: Mailtrap/local SMTP
+        const transporter = nodemailer.createTransport({
+          host: process.env.EMAIL_HOST,
+          port: process.env.EMAIL_PORT,
+          auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+          }
+        });
+        await transporter.sendMail(mailOptions);
+      }
+    } catch (err) {
+      console.error('Email send error:', err.response ? err.response.body : err);
+      throw new Error('There was an error sending the email. Try again later!');
     }
   }
 
