9f5eb5a6c5ba251526cd283a763908c9e329f631
madiha <madihaelyazal29@gmail.com>
2025-11-11 16:56:10 +0200

    commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,5 +1,4 @@
 const nodemailer = require('nodemailer');
-const { Resend } = require('resend');
 const fs = require('fs');
 const htmlToText = require('html-to-text');
 
@@ -8,43 +7,25 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = process.env.NODE_ENV === 'production' 
-      ? 'Metro Mate <onboarding@resend.dev>'  
-      : `Metro Mate <${process.env.EMAIL_FROM}>`; 
+    this.from = `Metro Mate <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      const resend = new Resend(process.env.RESEND_API_KEY);
-      return {
-        sendMail: async (mailOptions) => {
-          await resend.emails.send({
-            from: 'Metro Mate <onboarding@resend.dev>',
-            to: mailOptions.to,
-            subject: mailOptions.subject,
-            html: mailOptions.html,
-            text: mailOptions.text
-          });
-        }
-      }
-    }
-
-    // Development (Mailtrap)
     return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      service: 'Gmail',
       auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_APP_PASSWORD
       }
     });
   }
+
   async send(template, subject) {
     let html = fs.readFileSync(`${__dirname}/../views/emails/${template}.html`, 'utf-8');
     html = html.replace('{{firstName}}', this.firstName).replace('{{url}}', this.url);
     
     const mailOptions = {
-      from: this.from, 
+      from: this.from,
       to: this.to,
       subject,
       html,
