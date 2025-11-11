cfceada7ac18b0ddcd3c0edff6a9d3627b1bddad
madiha <madihaelyazal29@gmail.com>
2025-11-11 18:13:24 +0200

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,38 +1,33 @@
-const nodemailer = require('nodemailer');
 const fs = require('fs');
 const htmlToText = require('html-to-text');
+const { MailerSend, EmailParams, Recipient, Sender } = require("mailersend");
+
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Metro Mate <${process.env.EMAIL_FROM}>`;
-  }
-  newTransport() {
-    return nodemailer.createTransport({
-      host: 'smtp-relay.brevo.com',
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.BREVO_EMAIL,
-        pass: process.env.BREVO_STEMP_KEY
-      }
-    });
+    this.from = process.env.EMAIL_FROM;
   }
 
   async send(template, subject) {
     let html = fs.readFileSync(`${__dirname}/../views/emails/${template}.html`, 'utf-8');
     html = html.replace('{{firstName}}', this.firstName).replace('{{url}}', this.url);
-    
-    const mailOptions = {
-      from: this.from,
-      to: this.to,
-      subject,
-      html,
-      text: htmlToText.convert(html)
-    };
 
-    await this.newTransport().sendMail(mailOptions);
+    const mailerSend = new MailerSend({
+      api_key: process.env.MAILERSEND_API_KEY
+    });
+
+    const recipients = [ new Recipient(this.to) ];
+
+    const emailParams = new EmailParams()
+      .setFrom(new Sender(this.from, "Metro Mate"))
+      .setTo(recipients)
+      .setSubject(subject)
+      .setHtml(html)
+      .setText(htmlToText.convert(html));
+
+    await mailerSend.email.send(emailParams);
   }
 
   async sendWelcome() {
