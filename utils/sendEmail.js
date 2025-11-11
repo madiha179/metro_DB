b8105024a8c59d8ce82c4d5fe1324ef5d4c47368
madiha <madihaelyazal29@gmail.com>
2025-11-11 22:06:44 +0200

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,64 +1,51 @@
-const nodemailer = require('nodemailer');
-const { Resend } = require('resend');
 const fs = require('fs');
 const htmlToText = require('html-to-text');
+const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = process.env.NODE_ENV === 'production' 
-      ? 'Metro Mate <onboarding@resend.dev>'  
-      : `Metro Mate <${process.env.EMAIL_FROM}>`; 
+    this.from = process.env.EMAIL_FROM;
   }
 
-  newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      const resend = new Resend(process.env.RESEND_API_KEY);
-      return {
-        sendMail: async (mailOptions) => {
-          await resend.emails.send({
-            from: 'Metro Mate <onboarding@resend.dev>',
-            to: mailOptions.to,
-            subject: mailOptions.subject,
-            html: mailOptions.html,
-            text: mailOptions.text
-          });
-        }
-      }
-    }
-
-    // Development (Mailtrap)
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
-  }
   async send(template, subject) {
     let html = fs.readFileSync(`${__dirname}/../views/emails/${template}.html`, 'utf-8');
     html = html.replace('{{firstName}}', this.firstName).replace('{{url}}', this.url);
-    
+
     const mailOptions = {
-      from: this.from, 
       to: this.to,
+      from: this.from,
       subject,
       html,
       text: htmlToText.convert(html)
     };
 
-    await this.newTransport().sendMail(mailOptions);
+    try {
+      if (process.env.NODE_ENV === 'production') {
+        // SendGrid Web API
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        await sgMail.send(mailOptions);
+      } else {
+        // for local test NODE_ENV ===development
+        const transporter = nodemailer.createTransport({
+          host: process.env.EMAIL_HOST,
+          port: process.env.EMAIL_PORT,
+          auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+          }
+        });
+        await transporter.sendMail(mailOptions);
+      }
+    } catch (err) {
+      console.error('Email send error:', err.response ? err.response.body : err);
+      throw new Error('There was an error sending the email. Try again later!');
+    }
   }
-
-  async sendWelcome() {
-    await this.send('Welcome', 'Welcome To Metro Mate App');
-  }
-
-  async sendResetPassword() {
+ async sendResetPassword() {
     await this.send('resetPassEmail', 'Your Password reset Token valid for only 10 minutes');
   }
 };
