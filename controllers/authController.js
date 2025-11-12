7e681f4a79b53d68867d70beda505683921b904f
madiha <madihaelyazal29@gmail.com>
2025-11-12 22:21:24 +0200

    reset password functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,18 @@
 const User=require('./../models/usermodel');
 const jwt=require('jsonwebtoken');
+const crypto=require('crypto');
 const AppError=require('./../utils/appError');
 const CatchAsync=require('./../utils/catchAsyncError');
 const filterObj=require('./../utils/filterObject');
-const Email=require('./../utils/sendEmail')
+const Email=require('./../utils/sendEmail');
+const catchAsyncError = require('./../utils/catchAsyncError');
 //sign token
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRESIN
   });
 };
+// it create token to user when sigup ,login, resetpass
 const createSendToken=(user,statusCode,res)=>{
   const token=signToken(user._id);
   const cookieOptions={
@@ -66,7 +69,26 @@ exports.forgotPassword = CatchAsync(async (req,res,next)=>{
     user.passwordResetToken=undefined;
     user.passwordResetExpires=undefined;
      await user.save({ validateBeforeSave: false});
+      console.log("EMAIL ERROR:", err);
+  console.log("RESPONSE:", err.response);
+  console.log(" MESSAGE:", err.message);
      return next(new AppError('There was an error sending the email. Try again later!', 500));
   }
 });
-exports.resetPassword ={};
+exports.resetPassword =catchAsyncError(async(req,res,next)=>{
+  //1- get user based on token
+  const hashToken =crypto.createHash('sha256')
+  .update(req.params.token)
+  .digest('hex');
+  const user=await User.findOne({passwordResetToken:hashToken,passwordResetExpires:{$gt:Date.now()}});
+  //2- token has not Expired => user and set new pass
+  if(!user){
+    return next(new AppError('Token is invalid or expired'))}
+    user.password=req.body.password,
+    user.confirm_password=req.body.confirm_password,
+    user.passwordResetToken=undefined,
+    user.passwordResetExpires=undefined
+    await user.save();
+    //3-log user in 
+    createSendToken(user,200,res);
+});
--- models/usermodel.js
+++ models/usermodel.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 const validator=require('validator');
 const bcrypt=require('bcryptjs');
+const crypto=require('crypto');
 const UserSchema=new mongoose.Schema({
     ssn:{
         type:Number
@@ -57,7 +58,11 @@ const UserSchema=new mongoose.Schema({
     gender:{
         type:String,
         enum: ['male', 'female']
-    }
+    },
+    passwordChangedAt: Date,
+    passwordResetToken:String,
+    passwordResetExpires:Date,
+
 });
 //Hash password before saving 
 UserSchema.pre('save',async function (next){
@@ -67,8 +72,23 @@ UserSchema.pre('save',async function (next){
     this.confirm_password=undefined;
     next();
 });
+//change password time middelware
+UserSchema.pre('save',function(next){
+  if(!this.isModified('password')||this.isNew) return next();
+  this.passwordChangedAt = Date.now()-1000;
+  next();
+});
 // compare passwords
 UserSchema.methods.correctPassword=async function (candidatepassword,password) {
     return await bcrypt.compare(candidatepassword,password);}
+    //create Password Reset Token
+    UserSchema.methods.createPasswordResetToken = function() {
+      const resetToken= crypto.randomBytes(32).toString('hex');
+      this.passwordResetToken=crypto.createHash('sha256')
+      .update(resetToken)
+      .digest('hex');
+      this.passwordResetExpires=Date.now()+10*60*1000;
+      return resetToken;
+    }
 const User=mongoose.model('User',UserSchema);
 module.exports=User;
