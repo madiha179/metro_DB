8b1e20130747098f5e01fab67ea34143c31ddf0c
Rahma <rahmanasser9626@gmail.com>
2025-11-18 20:43:58 +0200

    Update some login error

--- controllers/authController.js
+++ controllers/authController.js
@@ -103,14 +103,10 @@ exports.Login =CatchAsync(async (req, res, next) => {
   
   const user = await User.findOne({email}).select('+password');
   
-  if(!user || !(await User.correctPassword(password, user.password)))
+  if(!user || !(await user.correctPassword(password, user.password)))
     return next(new AppError('Incorrect email or password!', 401));
   
-  const token = signToken(user.id);
-  res.status(200).json({
-    status: 'success',
-    token
-  });
+  createSendToken(user, 200, res);
 });
 exports.protect=CatchAsync(async(req,res,next)=>{
   //1- get token and check 
