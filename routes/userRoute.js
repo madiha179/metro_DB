a6c9d237fb33d2155df7ba887a0d7de404215846
madiha <madihaelyazal29@gmail.com>
2025-12-13 16:35:30 +0200

    some updates for profile apis

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -18,9 +18,8 @@ userRouter.patch('/changepassword',authcontroller.protect,authcontroller.changeP
 userRouter.get('/profile/username',authcontroller.protect,userController.getUserByName);
 userRouter.get('/profile/email',authcontroller.protect,userController.getUserEmail);
 userRouter.get('/profile/photo',authcontroller.protect,userController.getUserPhoto);
-userRouter.get('/profile/verified',authcontroller.protect,userController.getUserVerified);
-userRouter.put('/profile/updateuserphoto',authcontroller.protect,userController.updateuserphoto);
-userRouter.put('/profile/updateusername',authcontroller.protect,userController.updateusername);
+userRouter.patch('/profile/updateuserphoto',authcontroller.protect,userController.updateuserphoto);
+userRouter.patch('/profile/updateusername',authcontroller.protect,userController.updateusername);
 userRouter.get('/profile/getpaymentmethod',authcontroller.protect,userController.getPaymentMethod);
 userRouter.get('/profile/getbalance',authcontroller.protect,userController.getWalletBalance);
 module.exports=userRouter;
