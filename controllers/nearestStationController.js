2a270593edb46e059ee8ad262c22b955841e1abd
madiha <madihaelyazal29@gmail.com>
2026-02-15 20:50:21 +0200

    update

--- controllers/nearestStationController.js
+++ controllers/nearestStationController.js
@@ -2,36 +2,29 @@ const CatchAsync = require('../utils/catchAsyncError')
 const AppError = require('../utils/appError');
 const stationLocation = require('../models/stationsLocation');
 exports.getSatationWithIn = CatchAsync(async (req, res, next) => {
-  const { distance, lat, lng, unit } = req.params;
-  if (!lat || !lng || !distance) {
-    return next(new AppError('Please provide latitude, longitude and distance', 400));
-  }
-  
-  if (unit !== 'km' && unit !== 'mi') {
-    return next(new AppError('Unit must be km or mi', 400));
+  const { lat, lng } = req.params;
+  if (!lat || !lng) {
+    return next(new AppError('Please provide latitude and longitude', 400));
   }
   const latNum = parseFloat(lat);
   const lngNum = parseFloat(lng);
-  const distanceNum = parseFloat(distance);
 
-  if (isNaN(latNum) || isNaN(lngNum) || isNaN(distanceNum)) {
-    return next(new AppError('Invalid latitude, longitude or distance', 400));
+  if (isNaN(latNum) || isNaN(lngNum)) {
+    return next(new AppError('Invalid latitude or longitude', 400));
   }
-  const distanceInMeters = unit === 'mi' ? distanceNum * 1609.34 : distanceNum * 1000;
-
   const nearestStation = await stationLocation.findOne({
     location: {
       $nearSphere: {
         $geometry: {
           type: "Point",
           coordinates: [lngNum, latNum]
-        },
+        }
       }
     }
   });
 
   if (!nearestStation) {
-    return next(new AppError('No station found within the specified distance', 404));
+    return next(new AppError('No station found', 404));
   }
 
   res.status(200).json({
--- routes/nearestStationRoute.js
+++ routes/nearestStationRoute.js
@@ -1,5 +1,5 @@
 const express=require('express');
 const nearestStationsController=require('../controllers/nearestStationController');
 const nearestStationRoute=express.Router();
-nearestStationRoute.get('/:distance/:lat/:lng/:unit', nearestStationsController.getSatationWithIn);
+nearestStationRoute.get('/:lat/:lng', nearestStationsController.getSatationWithIn);
 module.exports=nearestStationRoute;
