2e4fce3f9ca29da06ee7a3303e11e82b96a14765
madiha <madihaelyazal29@gmail.com>
2026-02-15 20:15:12 +0200

    Update nearsetStationRoute.js

--- routes/nearsetStationRoute.js
+++ routes/nearsetStationRoute.js
@@ -1,5 +1,5 @@
 const express=require('express');
 const nearestStationsController=require('../controllers/nearestStationController');
 const nearestStationRoute=express.Router();
-nearestStationRoute.get('/:distance/center/:lat,:lng/unit/:unit', nearestStationsController.getSatationWithIn);
+nearestStationRoute.get('/:distance/:lat,:lng/:unit', nearestStationsController.getSatationWithIn);
 module.exports=nearestStationRoute;
