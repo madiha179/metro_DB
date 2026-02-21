dc7ab95af5094c3b31c5d46fe969ea87bf83fa1f
madiha <madihaelyazal29@gmail.com>
2026-02-22 01:03:19 +0200

    Update stationsDashDocs.js

--- swagger/stationsDashDocs.js
+++ swagger/stationsDashDocs.js
@@ -4,17 +4,24 @@
  *   - name: Stations DashBoard
  *     description: Stations CRUD Operations Management API
  */
+
 /**
  * @swagger
  * /api/v1/dashboard/getallstations:
  *   get:
- *     summary: Get Stations sorted by it is position in each line
+ *     summary: Get Stations sorted by position in each line
  *     tags: [Stations DashBoard]
  *     security:
  *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *         description: Page number
  *     responses:
  *       200:
- *         description:Return Stations data each 10 stations in a single page
+ *         description: Return Stations data each 10 stations in a single page
  *       404:
  *         description: No Stations To Show
  *       401:
@@ -25,17 +32,24 @@
 
 /**
  * @swagger
- * /api/v1/dashboard/getstation/:id:
+ * /api/v1/dashboard/getstation/{id}:
  *   get:
- *     summary: Get Station data by id 
+ *     summary: Get Station data by id
  *     tags: [Stations DashBoard]
  *     security:
  *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Station ID
  *     responses:
  *       200:
- *         description:Return Station data
+ *         description: Return Station data
  *       404:
- *         description:Station Not Found
+ *         description: Station Not Found
  *       401:
  *         description: Unauthorized - Invalid or missing token
  *       500:
@@ -46,7 +60,7 @@
  * @swagger
  * /api/v1/dashboard/addstation:
  *   post:
- *     summary: Add New Stataion
+ *     summary: Add New Station
  *     tags: [Stations DashBoard]
  *     security:
  *       - bearerAuth: []
@@ -57,25 +71,34 @@
  *           schema:
  *             type: object
  *             required:
- *               - station name
- *               - line number of station
- *               - station position
- *               - is_transfer
- *               - transfer_to
+ *               - stationName
+ *               - lineNumber
+ *               - position
  *             properties:
- *               station name:
+ *               stationName:
  *                 type: string
- *               linenumber :
+ *                 example: ain shams university
+ *               lineNumber:
  *                 type: number
- *               station position:
+ *                 example: 1
+ *               position:
  *                 type: number
- *               is_transfer :
+ *                 example: 35
+ *               isTransfer:
  *                 type: boolean
- *               transfer_to:
- *                 type: boolean
+ *                 example: false
+ *               transferTo:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     line:
+ *                       type: number
+ *                     position:
+ *                       type: number
  *     responses:
  *       201:
- *         description:Station Added 
+ *         description: Station Added
  *       401:
  *         description: Unauthorized - Invalid or missing token
  *       500:
@@ -84,59 +107,78 @@
 
 /**
  * @swagger
- * /api/v1/dashboard/updatestation/:id:
+ * /api/v1/dashboard/updatestation/{id}:
  *   patch:
- *     summary: Update Stataion Data
+ *     summary: Update Station Data
  *     tags: [Stations DashBoard]
  *     security:
  *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Station ID
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
  *             type: object
- *             required:
- *               - station name
- *               - line number of station
- *               - station position
- *               - is_transfer
- *               - transfer_to
  *             properties:
- *               station name:
+ *               stationName:
  *                 type: string
- *               linenumber :
+ *                 example: ain shams university
+ *               lineNumber:
  *                 type: number
- *               station position:
+ *                 example: 1
+ *               position:
  *                 type: number
- *               is_transfer :
+ *                 example: 36
+ *               isTransfer:
  *                 type: boolean
- *               transfer_to:
- *                 type: boolean
+ *                 example: false
+ *               transferTo:
+ *                 type: array
+ *                 items:
+ *                   type: object
+ *                   properties:
+ *                     line:
+ *                       type: number
+ *                     position:
+ *                       type: number
  *     responses:
- *       201:
- *         description:Station Updated  
+ *       200:
+ *         description: Station Updated
  *       401:
  *         description: Unauthorized - Invalid or missing token
  *       404:
- *         description:Station Not Found
+ *         description: Station Not Found
  *       500:
  *         description: Server error
  */
 
 /**
  * @swagger
- * /api/v1/dashboard/deletestation/:id:
+ * /api/v1/dashboard/deletestation/{id}:
  *   delete:
- *     summary: delete Station data by id 
+ *     summary: Delete Station by id
  *     tags: [Stations DashBoard]
  *     security:
  *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Station ID
  *     responses:
  *       204:
- *         description:station delete
+ *         description: Station Deleted
  *       404:
- *         description:Station Not Found
+ *         description: Station Not Found
  *       401:
  *         description: Unauthorized - Invalid or missing token
  *       500:
