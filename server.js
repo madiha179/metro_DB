c00ac13ddbe7a88505c08b885a2167e3d489c5a3
madiha <madihaelyazal29@gmail.com>
2026-02-21 17:05:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -44,9 +44,8 @@ mongoose.connect(DB,{
 })
 .then(async () => {
   console.log("✅ DB connection successful");
-  await createDefaultAdmin();
-  await Admin.updateMany({}, { $unset: { request: "" } });
-console.log("request field removed");
+ await Admin.deleteMany({ email: "metromate534@gmail.com" });
+await createDefaultAdmin();
 })
 .catch(err => console.error("❌ DB connection error:", err));
 const limiter=rateLimit({
