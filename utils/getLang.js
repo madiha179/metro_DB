c4634f9d4f21c70cf8d33f517ce40536b11ea2e3
madiha <madihaelyazal29@gmail.com>
2026-03-05 02:29:10 +0200

    Update getLang.js

--- utils/getLang.js
+++ utils/getLang.js
@@ -1,5 +1,5 @@
 module.exports=(req)=>{
   const query=req.query.lang;
-  const header=req.header['accept-language']?.split(',')[0]?.split('-')[0];
+  const header=req.headers['accept-language']?.split(',')[0]?.split('-')[0];
   return(query||header)==='ar'?'ar':'en';
 };
