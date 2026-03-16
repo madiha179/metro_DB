f9250392d65497e5aab4183f6cfd2465bb3e775c
madiha <madihaelyazal29@gmail.com>
2026-03-16 15:59:50 +0200

    Update userTripHistoryController.js

--- controllers/userTripHistoryController.js
+++ controllers/userTripHistoryController.js
@@ -4,17 +4,19 @@ const AppError=require('../utils/appError');
 const getLang=require('../utils/getLang');
 exports.userTripsHistory=catchAsyncError(async(req,res,next)=>{
  const lang =getLang(req);
- const userTrip=await userTrips.find({userId:req.user.id});
- if(!userTrip){
+ const userTrip_docs=await userTrips.find({userId:req.user.id});
+ if(!userTrip_docs){
   return next(new AppError('No trip history found',404));
  }
- const formattedHistory=userTrip.trip_history.map(trip=>({
-  fromStation:trip.fromStation[lang]||trip.fromStation.en,
-  toStation:trip.toStation[lang]||trip.toStation.en,
-  totalPrice:trip.totalPrice,
-  trip_date:trip.trip_date.toISOString().split('T')[0],
-  payment_method:trip.payment_method
- }));
+const formattedHistory=userTrip_docs.flatMap(doc=>
+  doc.trip_history.map(trip=>({
+    fromStation:trip.fromStation[lang]||trip.fromStation.en,
+    toStation:trip.toStation[lang]||trip.toStation.en,
+    totalPrice:trip.totalPrice,
+    trip_date:trip.trip_date.toISOString().split('T')[0],
+    payment_method:trip.payment_method
+  }))
+);
  res.status(200).json({
   status:'success',
   data:formattedHistory
