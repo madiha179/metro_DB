10606f786bfd602c9016c2863edbd691afcc91eb
Rahma Nasser <168233061+Rahma9999@users.noreply.github.com>
2026-04-30 15:23:33 +0300

    updates routes name again

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const authcontroller=require('./../controllers/authController');
-const adminController=require('../controllers/adminAuthConroller');
+const authcontroller = require('./../controllers/authController');
+const adminController = require('../controllers/adminAuthConroller');
 const { handleUploadErrors } = require('../utils/uploadMiddleware');
 const { createSubscription, getMySubscription, displaySubPlans, displaySubCategory } = require('../controllers/subscriptionController');
 const { getAllSubscriptions, updateSubStatus, getSubDoc } = require('../controllers/subscriptionDashController');
 const SubscriptionRouter = express.Router();
+const AdminSubRouter = express.Router();
 
 SubscriptionRouter.get('/plans', displaySubPlans);
 SubscriptionRouter.get('/plans/:category', displaySubCategory);
@@ -12,8 +13,8 @@ SubscriptionRouter.post('/create', authcontroller.protect, handleUploadErrors, c
 SubscriptionRouter.get('/me', authcontroller.protect, getMySubscription);
 
 /////////////Admin////////////////
-SubscriptionRouter.get('/', adminController.protect, getAllSubscriptions);
-SubscriptionRouter.patch('/:id/status', adminController.protect, updateSubStatus);
-SubscriptionRouter.get('/:id/documents/:docType', adminController.protect, getSubDoc);
+AdminSubRouter.get('/', adminController.protect, getAllSubscriptions);
+AdminSubRouter.patch('/:id/status', adminController.protect, updateSubStatus);
+AdminSubRouter.get('/:id/documents/:docType', adminController.protect, getSubDoc);
 
-module.exports = SubscriptionRouter;
+module.exports = {SubscriptionRouter, AdminSubRouter};
