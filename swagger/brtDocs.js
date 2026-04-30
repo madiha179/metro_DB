266251d5a83bab5232a43f4a384015147c3c6832
madiha <madihaelyazal29@gmail.com>
2026-05-01 00:38:59 +0300

    add brt swagger docs

---
+++ swagger/brtDocs.js
@@ -0,0 +1,44 @@
+/**
+ * @swagger
+ * /api/v1/brt/allstations:
+ *   get:
+ *     summary: Get all BRT stations
+ *     tags: [BRT]
+ *     responses:
+ *       200:
+ *         description: Return all BRT stations sorted by position
+ *       400:
+ *         description: Stations not found
+ *       500:
+ *         description: Server error
+ */
+
+/**
+ * @swagger
+ * /api/v1/brt/route:
+ *   get:
+ *     summary: Get route between two BRT stations
+ *     tags: [BRT]
+ *     parameters:
+ *       - in: query
+ *         name: startStation
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Name of the start station
+ *       - in: query
+ *         name: endStation
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Name of the end station
+ *     responses:
+ *       200:
+ *         description: Return route details between two stations
+ *       400:
+ *         description: Missing stations or same station selected
+ *       404:
+ *         description: One or both stations not found
+ *       500:
+ *         description: Server error
+ */
