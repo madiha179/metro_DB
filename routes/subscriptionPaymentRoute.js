cc3c05fa45c675a6d3e6c2857a931e717c093d7e
madiha <madihaelyazal29@gmail.com>
2026-05-12 18:27:27 +0300

    update validator

--- routes/subscriptionPaymentRoute.js
+++ routes/subscriptionPaymentRoute.js
@@ -6,7 +6,7 @@ const validateRequest=require('../utils/requestValidation');
 const subPaymentRoute=express.Router();
 subPaymentRoute.use(authController.protect);
 subPaymentRoute.post('/subscription-pay',[body('subscriptionId').notEmpty().withMessage('subscriptionId is required').isMongoId().withMessage('Invalid subscriptionId'),
-  body('paymentmethod').notEmpty().withMessage('payment method is required').isIn(['cash', 'visa card']).withMessage('Invalid payment method')]
+  body('paymentMethod').notEmpty().withMessage('payment method is required').isIn(['cash', 'visa card']).withMessage('Invalid payment method')]
 ,subPaymentController.subPaymentController);
 subPaymentRoute.post('/subscription-pay/visa',subPaymentController.visaPayController);
 subPaymentRoute.get('/subscription-pay/status',subPaymentController.getStatus);
