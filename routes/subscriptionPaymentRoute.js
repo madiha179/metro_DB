94ca00732e17ddbad14657d0b7b6b2fb4113a8b7
madiha <madihaelyazal29@gmail.com>
2026-05-12 18:25:24 +0300

    update validator

--- routes/subscriptionPaymentRoute.js
+++ routes/subscriptionPaymentRoute.js
@@ -1,9 +1,13 @@
 const express=require('express');
 const subPaymentController=require('./../controllers/subscriptionPaymentController');
 const authController=require('../controllers/authController');
+const {body}=require('express-validator');
+const validateRequest=require('../utils/requestValidation');
 const subPaymentRoute=express.Router();
 subPaymentRoute.use(authController.protect);
-subPaymentRoute.post('/subscription-pay',subPaymentController.subPaymentController);
+subPaymentRoute.post('/subscription-pay',[body('subscriptionId').notEmpty().withMessage('subscriptionId is required').isMongoId().withMessage('Invalid subscriptionId'),
+  body('paymentmethod').notEmpty().withMessage('payment method is required').isIn(['cash', 'visa card']).withMessage('Invalid payment method')]
+,subPaymentController.subPaymentController);
 subPaymentRoute.post('/subscription-pay/visa',subPaymentController.visaPayController);
 subPaymentRoute.get('/subscription-pay/status',subPaymentController.getStatus);
 module.exports=subPaymentRoute;
